e01bcbe20e4325e5868bf85a7bea157d97843fd8
dadak797 <dadak797@gmail.com>
2023-09-16 16:02:42 +0900

    4.6

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,18 @@ const Home: NextPage = () => {
   return (
     <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-2xl">
-          Select Item
-        </span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Grey Chair</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Tolly Table</span>
-          <span className="font-semibold">$19</span>
-        </div>
+        <span className="font-semibold text-2xl">Select Item</span>
+        <ul>
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-50">
+              <span className="text-gray-500">Grey Chair</span>
+              <span className="font-semibold">$19</span>
+            </div>
+          ))}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((c, i) => <li className="bg-red-500 py-2 empty:hidden" key={i}>{c}</li>)}
+        </ul> 
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
