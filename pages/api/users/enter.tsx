85bb46bd4175734e4c11685c0010ca8686e510b2
dadak797 <dadak797@gmail.com>
2023-09-23 09:39:10 +0900

    9.2

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,56 +6,24 @@ async function handler (req: NextApiRequest, res: NextApiResponse)
 {
     const { phone, email } = req.body;
     const payload = phone ? { phone: +phone } : { email };
-    const user = await client.user.upsert({
-        where: {
-            ...payload
-        },
-        create: {
-            name: "Anonymous",
-            ...payload,
-        },
-        update: {},
+    const token = await client.token.create({
+        data: {
+            payload: "1234",
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...payload
+                    },
+                    create: {
+                        name: "Anonymous",
+                        ...payload,
+                    },
+                }
+            }
+        }
     });
-    console.log(user);
+    console.log(token);
 
-    // if (email) {
-    //     user = await client.user.findUnique({
-    //         where: {
-    //             email,
-    //         },
-    //     });
-    //     if (user) console.log("found it.");
-    //     if (!user) {
-    //         console.log("Did not find. Will create.");
-    //         user = await client.user.create({
-    //             data: {
-    //                 name: "Anonymous",
-    //                 email,
-    //             },
-    //         });
-    //     }
-    //     console.log(user);
-    // }
-
-    // if (phone) {
-    //     user = await client.user.findUnique({
-    //         where: {
-    //             phone: +phone,
-    //         },
-    //     });
-    //     if (user) console.log("found it.");
-    //     if (!user) {
-    //         console.log("Did not find. Will create.");
-    //         user = await client.user.create({
-    //             data: {
-    //                 name: "Anonymous",
-    //                 phone: +phone,
-    //             },
-    //         });
-    //     }
-    //     console.log(user);
-    // }
-    
     res.status(200).end();
 }
 
