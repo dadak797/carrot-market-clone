be5601af8a3b0511bac2697e242e028991418b8d
dadak797 <dadak797@gmail.com>
2023-09-23 10:01:24 +0900

    9.2 - modified

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,25 +5,25 @@ import withHandler from "@/libs/server/withHandler";
 async function handler (req: NextApiRequest, res: NextApiResponse)
 {
     const { phone, email } = req.body;
-    const payload = phone ? { phone: +phone } : { email };
+    const user = phone ? { phone: +phone } : { email };
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
     const token = await client.token.create({
-        data: {
-            payload: "1234",
-            user: {
-                connectOrCreate: {
-                    where: {
-                        ...payload
-                    },
-                    create: {
-                        name: "Anonymous",
-                        ...payload,
-                    },
-                }
-            }
-        }
+      data: {
+        payload,
+        user: {
+          connectOrCreate: {
+            where: {
+              ...user,
+            },
+            create: {
+              name: "Anonymous",
+              ...user,
+            },
+          },
+        },
+      },
     });
-    console.log(token);
-
+    
     res.status(200).end();
 }
 
