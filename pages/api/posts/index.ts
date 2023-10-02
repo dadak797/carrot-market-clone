ae43410c8c8a5f2ab5c64d28ff456fcc35a0137b
dadak797 <dadak797@gmail.com>
2023-10-03 00:37:48 +0900

    12.6

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -10,13 +10,15 @@ async function handler(
 ) {
 
     const { 
-        body: { question }, 
+        body: { question, latitude, longitude }, 
         session: { user },
     } = req;
     if (req.method === "POST") {
         const post = await client.post.create({
             data: {
                 question,
+                latitude,
+                longitude,
                 user: {
                     connect: {
                         id: user?.id,
