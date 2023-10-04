df9128be84a16ead8b26bb0dd45a797e40acf108
dadak797 <dadak797@gmail.com>
2023-10-04 14:46:59 +0900

    14.0

--- components/layout.tsx
+++ components/layout.tsx
@@ -114,9 +114,9 @@ export default function Layout({
               </svg>
               <span>채팅</span>
           </Link>
-          <Link href="/live"               className={cls(
+          <Link href="/streams" className={cls(
                 "flex flex-col items-center space-y-2 ",
-                router.pathname === "/live"
+                router.pathname === "/streams"
                   ? "text-orange-500"
                   : "hover:text-gray-500 transition-colors"
               )}>
