cdd6af2f1effb2a2408bd0faaf43e094b7adbae0
dadak797 <dadak797@gmail.com>
2023-10-05 14:35:56 +0900

    14.4

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -37,7 +37,10 @@ const Stream_: NextPage = () => {
   const router = useRouter();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000,
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -45,13 +48,28 @@ const Stream_: NextPage = () => {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate((prev) => 
+      prev && 
+        ({
+          ...prev, 
+          stream: {
+            ...prev.stream, 
+            messages: [
+              ...prev.stream.messages, 
+              {
+                id: Date.now(), 
+                message: form.message, 
+                user: {
+                  ...user
+                },
+              },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
