c2ae9b613fbd05cf60a088eed7ae8e640ac69e18
dadak797 <dadak797@gmail.com>
2023-10-10 16:41:31 +0900

    19.6

--- components/item.tsx
+++ components/item.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
 
 interface ItemProps {
   title: string;
   id: number;
   price: number;
+  image: string;
   hearts: number;
 }
 
@@ -12,11 +14,19 @@ export default function Item({
   price,
   hearts,
   id,
+  image,
 }: ItemProps) {
   return (
     <Link href={`/products/${id}`} className="flex px-4 pt-5 cursor-pointer justify-between">
         <div className="flex space-x-4">
-          <div className="w-20 h-20 bg-gray-400 rounded-md" />
+          <div className="w-20 h-20 relative  rounded-md">
+            <Image
+              src={`https://imagedelivery.net/-PN3rdNdPHiZc--j3mgNQw/${image}/public`}
+              layout="fill"
+              objectFit="cover"
+              alt="product"
+            />
+          </div>
           <div className="pt-2 flex flex-col">
             <h3 className="text-sm font-medium text-gray-900">{title}</h3>
             <span className="font-medium mt-1 text-gray-900">${price}</span>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="w-full max-w-xl mx-auto">
         <Component {...pageProps} />
       </div>
-      <Script
+      {/* <Script
         src="https://developers.kakao.com/sdk/js/kakao.js"
         strategy="lazyOnload"
       />
@@ -32,7 +32,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             });
           };
         }}
-      />
+      /> */}
     </SWRConfig>
   );
 }
