0e00f7bfdc090b162de1b0c00f59942a5a556146
dadak797 <dadak797@gmail.com>
2023-10-10 09:33:43 +0900

    19.1

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,19 @@
-import { NextFetchEvent, NextRequest } from "next/server";
+import { NextFetchEvent, NextRequest, NextResponse, userAgent } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    console.log("It works! Global middleware");
-    if (req.nextUrl.pathname.startsWith('/chats')) {
-        console.log(`this is chats ONLY middleware!`);
+    if (userAgent(req).isBot) {
+        return new Response("A bot cannot access to this website!", { status: 403 });
     }
+
+    if(!req.url.includes("/api")) {
+        if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+            return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+        }
+    }
+
+    // return NextResponse.json({ ok: true });
 }
+
+export const config = {
+    matcher: ["/((?!_next|api/auth).*)(.+)"],
+};
