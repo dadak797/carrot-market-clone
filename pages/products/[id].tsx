1701035ece1b09e571f7abc423aa07b3c899b575
dadak797 <dadak797@gmail.com>
2023-10-11 16:58:26 +0900

    20.5

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -41,6 +41,13 @@ const ItemDetail: NextPage<ItemDetailResponse> = ({
     // mutate("/api/users/me", (prev: any) => ({ ok: !prev.ok }), false);
     toggleFav({});
   };
+  if (router.isFallback) {
+    return (
+      <Layout title="Loading for youuuuuuu">
+        <span>I love you</span>
+      </Layout>
+    );
+  }
   return (
     <Layout canGoBack seoTitle="Product Detail">
       <div className="px-4 py-4">
@@ -135,7 +142,7 @@ const ItemDetail: NextPage<ItemDetailResponse> = ({
 export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
-    fallback: "blocking",
+    fallback: true,
   };
 };
 
